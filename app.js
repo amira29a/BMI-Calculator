46906e918f413da68d283d5155938f5cc341ec2d
Amira Elamir <56198841+amira29a@users.noreply.github.com>
2021-08-22 17:28:48 +0200

    add commint

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 (() => {
 
+    /* Global Variables */
     let bmiPosts = document.getElementById("bmiposts");
     let calcula = document.getElementById("calcula");
-    document.getElementById('bmiposts').style.display = 'none';
- 
- 
+    document.getElementById('bmiposts').style.display = 'none'; // display this elemnt to hide it 
+
    let formss = document.getElementById("formss");
    const form = document.getElementById("bmi");
    const heightValue = document.getElementById("heightValue");
    const weightValue = document.getElementById("weightValue");
  
- 
+//    addEventListener to the form to submin 
    form.addEventListener("submit", onSubmit, false);  
    function onSubmit(e) {
      e.preventDefault();
-     
      let [weight, height] = getBodyParams();
      if(!isNaN(calculateBMI(weight, height))){
      const bmi = calculateBMI(weight, height);
@@ -23,11 +22,11 @@
  
    }
    }
- 
+    // function to get value from the input 
    function getBodyParams() {
-     const $weightInput = form.querySelector("input[name=weight]");
-     const $heightInput = form.querySelector("input[name=height]");
-     
+     const $weightInput = form.querySelector("input[name=weight]");// input height
+     const $heightInput = form.querySelector("input[name=height]"); // input height
+      // return 
      return [$weightInput, $heightInput].map($input =>
        getInput($input)
      );
@@ -47,46 +46,50 @@
      if (weight && height) {        
        return (weight / (height / 100 * height / 100)).toFixed(1);
  
-     }
+     } // if the user click on the submit button without fill the fields will show the alert message
        else {
        calcula.innerHTML = `<p>من فضلك، املأ جميع الحقول واستخدم الأرقام فقط*</p>`;       
    }
-      }
+     }
  
- 
+   // function to show result with numbers and tips 
    function analyzeResult(bmi) {
    document.getElementById('calcula').style.display = 'none';
     const articals = form.querySelector(".articals");
  
      let healthMessage = '';
+     // if the resulte less than 18.5 then "its underweight" show this tips and result 
      if (bmi < 18.5) {
        healthMessage = "وزن ناقص";
-     } else if (bmi > 18.5 && bmi < 25) {
+       articals.innerHTML =` <h3 class ="tips"> نصائح شخصية </h3>
+       <p>ماذا</p>`;
+     }  // if the resulte bettwen 18.5 to 25 "healthy weight" show this tips and result 
+      else if (bmi > 18.5 && bmi < 25) {
        healthMessage = "وزن صحى ومثالى";
+       articals.innerHTML =` <h3 class ="tips"> نصائح شخصية </h3>
+       <p>ماذا</p>`;
      } 
+     // if the resulte bettwen 25 to 30 show this tips and result 
      else if (bmi > 25 && bmi < 30 ) {
        healthMessage = "وزن زائد";
               articals.innerHTML =` <h3 class ="tips"> نصائح شخصية </h3>
          <p>ماذا لو كان بإمكانك تناول الكعك و فقدان الوزن؟ يعدك النظام الغذائي الذي يسمى سيكو (CICO)؛ اختصار لمصطلح "السعرات الحرارية الداخلة، السعرات الحرارية الخارجة" بذلك.</p>
      `;
-     }
+     } // if not show this tips 
      else {
        healthMessage = "سمنه";
          articals.innerHTML =` <h3 class ="tips"> نصائح شخصية </h3>
          <p>ماذا لو كان بإمكانك تناول الكعك و فقدان الوزن؟ يعدك النظام الغذائي الذي يسمى سيكو (CICO)؛ اختصار لمصطلح "السعرات الحرارية الداخلة، السعرات الحرارية الخارجة" بذلك.</p>
          <p>مبدأ النظام الغذائي واضح جدًا: تناول كل ما تريد بما في ذلك من وجبات سريعة وأنقص وزنك طالما تحرق سعرات حرارية أكثر مما تستهلك كل يوم.</p>
          <p>لأنه بإمكانك تناول كل ما تريد و أي شئ تريد،حيث أنه كل ما يهم هو مجموع السعرات الحرارية، لا يوافق خبراء التغذية على هذه الحمية.</p>
-         <p>الصحة ليست عبارة عن مجرد فقدان الوزن وحده. عليك النظر إلى الحزمة بأكملها، قد تفقد الوزن فعلا من خلال اتباعك لهذا النظام الغذائي و لكن هناك جانب سلبي وهو نقص المغذيات أو حتى سوء التغذية.</p>
-         <p>يشير خبراء التغذية إلى أهمية تناول الأطعمة الصحية مثل البروكلي، جوز البيكان، التوت، المعكرونة وزيت الزيتون، بدلا من اتباع نظام غذائي مجنون.</p>
-         <p>يشير خبراء التغذية إلى أن أفضل حل لفقدان الوزن هو من خلال المزج بين الأكل الصحي و ممارسة الرياضة. قم بتخفيض كمية السعرات الحرارية عن طريق تقليل كمية و حجم الوجبة، بالإضافة إلى ممارسة الرياضة بانتظام بما لا يقل عن خمسة إلى ستة أيام في الأسبوع.</p>
-         <p>لإنقاص الوزن عن طريق ممارسة الرياضة قم بممارسة تمارين القلب والأوعية الدموية -مثل المشي بسرعة أو الجري و ركوب الدراجات والسباحة – وتمارين المقاومة.</p>
-            
-     `;
+            `;
  
      }
      
      return healthMessage;
-   }
+   }  
+   
+   // function to show the resulte with table
    function displayResult(bmi, msg) {
      const result = form.querySelector(".result");
      const health = form.querySelector(".health");
